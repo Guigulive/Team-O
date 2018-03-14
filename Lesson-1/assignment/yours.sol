4a93624b9bc3a72bd4919bc319fd70acd9d25f36
142-ZHANGJIA <git@mindgravity.cn>
2018-03-14 22:51:05 +0800

    第一课作业
    1、复盘课件内容
    2、传参实现可变地址可变薪水数

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,39 @@
 /*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint salary;//salary viable
+    address staffAddress;//address viable
+    uint constant payDuration = 10 seconds;
+    uint lastPayday = now;
+    
+    //return the balance of current contract
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+
+    //return salary afford times
+    function calculateRunway(uint salary) constant public returns (uint) {
+        return this.balance / salary;
+    }
+
+    //check affordability
+    function hasEnoughFund(uint salary) constant public returns (bool) {
+        return calculateRunway(salary) > 0;
+    }
+
+    //sender get his salary
+    function getPaid(address staffAddress, uint salary) public payable {
+        if(msg.sender != staffAddress) {
+            revert();
+        }
+        
+        uint nextPayday = lastPayday + payDuration;
+        if (nextPayday > now) {
+            revert();
+        }
+        
+        lastPayday = nextPayday;
+        staffAddress.transfer(salary);
+    }
+}
