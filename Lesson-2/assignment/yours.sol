2ec4f2869b0c40ee5e8a8a7a8430430b31169357
mww0595 <mww0595@gmail.com>
2018-03-20 06:59:27 +0800

    使用Mapping来代替数组实现Employee

--- Lesson-2/assignment/yours.sol
+++ Lesson-2/assignment/yours.sol
@@ -14,65 +14,55 @@ contract Payroll {
         uint lastPayDay;
     }
 
-    Employee[] employees;
+    // Employee[] employees;
+    mapping(address => Employee) employees;
 
     function _partialPay(Employee employee) private{
         uint payment = employee.salary * (now - employee.lastPayDay) * payDuration;
         employee.id.transfer(payment);
     }
 
-    function _findEmployee(address employeeID) private returns (Employee, uint){
-      for(uint i = 0; i < employees.length; i++){
-            if(employees[i].id == employeeID){
-                return (employees[i], i);
-            }
-        }
-        // return (0, 0);
+    function _findEmployee(address employeeID) private returns (Employee){
+        return employees[employeeID];
     }
 
     function addEmployee(address employeeID, uint salary){
         require(msg.sender == owner);
-        var (employee, index)  = _findEmployee(employeeID);
+        var employee  = _findEmployee(employeeID);
         assert(employee.id == 0x00);
-        employees.push(Employee(employeeID, salary * 1 ether, now));
+        employees[employeeID] = Employee(employeeID, salary * 1 ether, now);
         totalSalary += salary;
     }
 
 
     function removeEmployee(address employeeID){
         require(msg.sender == owner);
-
-        for(uint i = 0; i < employees.length; i++){
-            totalSalary -= employees[i].salary;
-            delete employees[i];
-            employees[i] = employees[employees.length -1];
-            employees.length -= 1;
-
-            return;
-        }
-
+        var employee = employees[employeeID];
+        assert(employee.id != 0x00);
+        _partialPay(employee);
+        delete employees[employeeID];
     }
 
     function updateEmployee(address employeeID, uint salary){
         require(msg.sender == owner);
 
-        var (e, index) = _findEmployee(employeeID);
+        var e = _findEmployee(employeeID);
         require(e.id != 0x00);
 
         _partialPay(e);
-        totalSalary -= employees[index].salary;
-        employees[index].id = employeeID;
-        employees[index].salary = salary;
+        totalSalary -= employees[employeeID].salary;
+        employees[employeeID].id = employeeID;
+        employees[employeeID].salary = salary;
         totalSalary += salary;
 
     }
 
     function getPaid() payable public{
-        var (e, index) = _findEmployee(msg.sender);
+        var e = _findEmployee(msg.sender);
         require(e.id != 0x00);
         require(e.lastPayDay + payDuration < now);
 
-        employees[index].lastPayDay = now;
+        e.lastPayDay = now;
         e.id.transfer(e.salary);
     }
 
@@ -92,7 +82,7 @@ contract Payroll {
         //     totalSalary += employees[i].salary;
         // }
 
-        require(totalSalary != 0);
+        // require(totalSalary != 0);
 
         return this.balance / totalSalary;
     }
