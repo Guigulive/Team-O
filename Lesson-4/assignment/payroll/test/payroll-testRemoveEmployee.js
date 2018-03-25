3fb21ae768ce45212411179ed669b21f56269b58
chaojie1022 <chaojie1022@gmail.com>
2018-03-25 22:50:51 +0800

    Create payroll-testRemoveEmployee.js

---
+++ Lesson-4/assignment/payroll/test/payroll-testRemoveEmployee.js
@@ -0,0 +1,51 @@
+var Payroll = artifacts.require("./Payroll.sol");
+
+contract('Payroll', function (accounts) {
+
+     const owner = accounts[0]
+     const employee = accounts[1]
+     const salary = 2;    
+	 
+     it("测试先增加员工再将该员工移出，在合约为owner的情况下", function () {
+		var payroll;
+		
+		return Payroll.deployed().then(instance => {
+			
+		payroll = instance;
+		
+		return payroll.addEmployee(employee, salary, { from: owner });
+		
+		}).then(() => {
+			
+			return payroll.removeEmployee(employee);
+		
+		}).then(() => {
+			
+			return payroll.employees(employee);
+			
+		}).then(employeeInfo => {
+			console.log(employeeInfo);
+			assert.equal(employeeInfo[0].toNumber(), 0, "移除员工失败");
+		});
+	});
+
+
+     it("测试移除一个并不存在的员工", function () {
+		 
+        var payroll;
+        return Payroll.deployed().then( instance => {
+			
+            payroll = instance;
+            
+			return payroll.removeEmployee(employee);
+        }).then(() => {
+            assert(false, "不能成功移出一个不存在的员工");
+        }).catch(error => {
+            assert.include(
+				error.toString(), 
+				"invalid opcode", 
+				"不能移出不存在的员工");
+        });
+     });
+
+}) 
