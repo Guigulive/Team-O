6d69e6489d5ddcd339c6c90a703ee9808075f8a3
guousa <guo.usa@qq.com>
2018-03-31 13:47:18 +0000

    add assignment

---
+++ Lesson-5/assignment/test/addEmployeeTest.js
@@ -0,0 +1,19 @@
+var Payroll = artifacts.require("./Payroll.sol");
+
+contract('Payroll', function(accounts) {
+
+  it("addEmployee function unit test.", function() {
+    return Payroll.deployed().then(function(instance) {
+      payrollInstance = instance;
+
+      console.log("employeeId1: ",accounts[1]);
+      return payrollInstance.addEmployee(accounts[1], 10, {from: accounts[0]});
+    }).then(function() {
+      console.log("employeeId2: ",accounts[1]);
+      return payrollInstance.checkEmployee.call(accounts[1]);
+    }).then(function(resultBool) { 
+      assert.equal(resultBool, true, "Adding employee is not successful.");
+    });
+  });
+
+});
